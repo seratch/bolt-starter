1d7e80c3cad4c9590032261f29ecfe025fd23ded
Kazuhiro Sera <seratch@gmail.com>
2020-02-15 14:29:25 +0900

    Update the code to use logger, client in params

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ for (const k in config) {
   process.env[k] = config[k];
 }
 
-const { LogLevel, ConsoleLogger } = require("@slack/logger");
+const { LogLevel } = require("@slack/logger");
 const logLevel = process.env.SLACK_LOG_LEVEL || LogLevel.DEBUG;
-const logger = new ConsoleLogger();
-logger.setLevel(logLevel);
 
 const { App, ExpressReceiver } = require("@slack/bolt");
 // Manually instantiate to add external routes afterwards
@@ -17,7 +15,6 @@ const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 const app = new App({
-  logger: logger,
   logLevel: logLevel,
   token: process.env.SLACK_BOT_TOKEN,
   receiver: receiver
@@ -28,7 +25,7 @@ const app = new App({
 // see https://slack.dev/bolt/
 
 // https://api.slack.com/apps/{APP_ID}/event-subscriptions
-app.event("app_mention", ({ event, say }) => {
+app.event("app_mention", ({ logger, event, say }) => {
   logger.debug(
     "app_mention event payload:\n\n" + JSON.stringify(event, null, 2) + "\n"
   );
@@ -39,84 +36,83 @@ app.event("app_mention", ({ event, say }) => {
 
 // https://api.slack.com/apps/{APP_ID}/slash-commands
 // https://api.slack.com/apps/{APP_ID}/interactive-messages
-app.command("/open-modal", ({ ack, body, context }) => {
-  app.client.views
-    .open({
-      "token": context.botToken,
-      "trigger_id": body.trigger_id,
-      // Block Kit Builder - http://j.mp/bolt-starter-modal-json
-      "view": {
-        "type": "modal",
-        "callback_id": "task-modal",
-        "private_metadata": JSON.stringify(body), // Remove this when pasting this in Block Kit Builder
-        "title": {
-          "type": "plain_text",
-          "text": "Create a task",
-          "emoji": true
-        },
-        "submit": {
-          "type": "plain_text",
-          "text": "Submit",
-          "emoji": true
-        },
-        "close": {
-          "type": "plain_text",
-          "text": "Cancel",
-          "emoji": true
-        },
-        "blocks": [
-          {
-            "type": "input",
-            "block_id": "input-title",
-            "element": {
-              "type": "plain_text_input",
-              "action_id": "input",
-              "initial_value": body.text // Remove this when pasting this in Block Kit Builder
-            },
-            "label": {
-              "type": "plain_text",
-              "text": "Title",
-              "emoji": true
-            },
-            "optional": false
+app.command("/open-modal", ({ logger, client, ack, body, context }) => {
+  client.views.open({
+    "token": context.botToken,
+    "trigger_id": body.trigger_id,
+    // Block Kit Builder - http://j.mp/bolt-starter-modal-json
+    "view": {
+      "type": "modal",
+      "callback_id": "task-modal",
+      "private_metadata": JSON.stringify(body), // Remove this when pasting this in Block Kit Builder
+      "title": {
+        "type": "plain_text",
+        "text": "Create a task",
+        "emoji": true
+      },
+      "submit": {
+        "type": "plain_text",
+        "text": "Submit",
+        "emoji": true
+      },
+      "close": {
+        "type": "plain_text",
+        "text": "Cancel",
+        "emoji": true
+      },
+      "blocks": [
+        {
+          "type": "input",
+          "block_id": "input-title",
+          "element": {
+            "type": "plain_text_input",
+            "action_id": "input",
+            "initial_value": body.text // Remove this when pasting this in Block Kit Builder
           },
-          {
-            "type": "input",
-            "block_id": "input-deadline",
-            "element": {
-              "type": "datepicker",
-              "action_id": "input",
-              "placeholder": {
-                "type": "plain_text",
-                "text": "Select a date",
-                "emoji": true
-              }
-            },
-            "label": {
-              "type": "plain_text",
-              "text": "Deadline",
-              "emoji": true
-            },
-            "optional": true
+          "label": {
+            "type": "plain_text",
+            "text": "Title",
+            "emoji": true
           },
-          {
-            "type": "input",
-            "block_id": "input-description",
-            "element": {
-              "type": "plain_text_input",
-              "action_id": "input",
-              "multiline": true
-            },
-            "label": {
+          "optional": false
+        },
+        {
+          "type": "input",
+          "block_id": "input-deadline",
+          "element": {
+            "type": "datepicker",
+            "action_id": "input",
+            "placeholder": {
               "type": "plain_text",
-              "text": "Description",
+              "text": "Select a date",
               "emoji": true
-            },
-            "optional": true
-          }
-        ]
-      }
-    })
+            }
+          },
+          "label": {
+            "type": "plain_text",
+            "text": "Deadline",
+            "emoji": true
+          },
+          "optional": true
+        },
+        {
+          "type": "input",
+          "block_id": "input-description",
+          "element": {
+            "type": "plain_text_input",
+            "action_id": "input",
+            "multiline": true
+          },
+          "label": {
+            "type": "plain_text",
+            "text": "Description",
+            "emoji": true
+          },
+          "optional": true
+        }
+      ]
+    }
+  })
     .then(res => {
       logger.debug(
         "views.open response:\n\n" + JSON.stringify(res, null, 2) + "\n"
@@ -129,7 +125,7 @@ app.command("/open-modal", ({ ack, body, context }) => {
     });
 });
 
-app.view("task-modal", async ({ body, ack }) => {
+app.view("task-modal", async ({ logger, body, ack }) => {
   logger.debug(
     "view_submission view payload:\n\n" +
     JSON.stringify(body.view, null, 2) +
@@ -157,6 +153,7 @@ app.view("task-modal", async ({ body, ack }) => {
     );
     // Post a message using response_url given by the slash comamnd
     const command = JSON.parse(body.view.private_metadata);
+    // Cannot use respond here as the response_url is not given here
     await postViaResponseUrl(
       command.response_url, // available for 30 minutes
       {
@@ -214,7 +211,7 @@ if (process.env.SLACK_REQUEST_LOG_ENABLED === "1") {
     }
     copiedArgs.client = {};
     copiedArgs.logger = {};
-    logger.debug(
+    args.logger.debug(
       "Dumping request data for debugging...\n\n" +
       JSON.stringify(copiedArgs, null, 2) +
       "\n"
